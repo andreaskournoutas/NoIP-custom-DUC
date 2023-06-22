91d6f0d3f7dc62c400dd460e8ca71e7e1a42ae97
Andreas Kournoutas <andreaskournoutas@gmail.com>
2023-06-22 22:31:53 +0300

    Removed formdata separation

--- scripts/createFormData.js
+++ scripts/createFormData.js
@@ -1,7 +1,5 @@
 function createFormData(form) {
     console.log('Creating formdata...');
     let formData = new FormData(form);
-    localStorage.setItem('hostname', formData.get('hostname'));
-    formData.delete('hostname');
     return formData;
 }
