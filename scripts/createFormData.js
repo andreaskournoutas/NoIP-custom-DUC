833bde43af0d835f7d49dc90b4f69b8789ca0574
Andreas Kournoutas <andreaskournoutas@gmail.com>
2023-06-23 08:19:14 +0300

    Revert "Removed formdata separation"
    This reverts commit 91d6f0d3f7dc62c400dd460e8ca71e7e1a42ae97.

--- scripts/createFormData.js
+++ scripts/createFormData.js
@@ -1,5 +1,7 @@
 function createFormData(form) {
     console.log('Creating formdata...');
     let formData = new FormData(form);
+    localStorage.setItem('hostname', formData.get('hostname'));
+    formData.delete('hostname');
     return formData;
 }
